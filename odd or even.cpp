87eddedd2e598e833404d6f4701558b4daff7e6c
Faheem Ullah <faheemullah.ims@gmail.com>
2026-02-08 19:50:35 +0500

    Add files via upload

---
+++ odd or even.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int number;
+	cout<<"Enter any number "<<endl;
+	cin>>number;
+	if(number%2==0)
+	{cout<<"This number us even ";
+	}
+	else{
+		cout<<"this number is odd ";
+	}
+	return 0;
+	
+	
+	
+	
+	
+	
+	
+	
+}
