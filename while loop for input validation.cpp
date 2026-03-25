2d4054faf335347e1481b50e659f4f8860c17381
Faheem Ullah <faheemullah.ims@gmail.com>
2026-03-25 22:05:19 +0500

    Update while loop for input validation.cpp

--- while loop for input validation.cpp
+++ while loop for input validation.cpp
@@ -6,7 +6,7 @@ int main()
 	cout<<"enter number of your choice: ";
 	cin>>range;
 	while(range<0 || range>10)//and can not be used bcz number can not be less than 0 and greater than at same time 
-	{						//and both conditions must be true or=1 of the 2 cindtion true condition true
+	{					 	//and both conditions must be true or=1 of the 2 cindtion true condition true
 		cout<<range<<" is out of range "<<endl;
 		cout<<"enter another number \n";
 		cin>>range;
