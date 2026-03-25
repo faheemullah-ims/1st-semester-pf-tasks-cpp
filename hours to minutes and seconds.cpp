f751aabcb41c86ca4b0e1ed65f42b819d5009d11
Faheem Ullah <faheemullah.ims@gmail.com>
2026-03-25 10:03:59 -0700

    Add files via upload

---
+++ hours to minutes and seconds.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a;
+	int b;
+	int result;
+	 
+	 cout<<"enter the value of a ";
+	 cin>>a;
+	 cout<<"enter the value of b ";
+	 cin>>b;
+	 
+	 result=((a*a)+(b*b)+2*a*b)/(a+b);
+	 cout<<"the result is "<<result<<endl;
+	 return 0;
+	 
+	
+	
+	
+	
+	
+}
