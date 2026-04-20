e6ca9eb89cdfb63d210ccdc49649b362412e59cc
Faheem Ullah <faheemullah.ims@gmail.com>
2026-04-21 00:04:06 +0500

    Fix formatting issues in BILL CALCULATIOM.cpp

--- BILL CALCULATIOM......cpp
+++ BILL CALCULATIOM......cpp
@@ -11,6 +11,7 @@ int main()
 	cout<<"meal coast is "<<mealcoast<<" rupees."<<endl;
 	cout<<"the sales tax is "<<tax<<" rupees."<<endl;
 	cout<<"mealcost and tax is "<<mealcoastandtax<<" rupees "<<endl;
+	
 	cout<<"the tip is "<<tip<<" rupees. "<<endl;
 	cout<<"the total bill is "<<total<<" rupees. "<<endl;
 	return 0;	
